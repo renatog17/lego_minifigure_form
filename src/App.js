9803a426186991fe8bc33cb4bc1f100ec44b4436
Renato Oliveira <renatog.oliveira17@gmail.com>
2023-06-20 15:22:00 -0300

    react props

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ function App() {
   return (
     <div className="App">
       <Banner/>
-      <CampoTexto/>
+      <CampoTexto label='Nome' placeholder='Digite o seu nome'/>
+      <CampoTexto label='Cargo' placeholder='Digite o seu cargo'/>
+      <CampoTexto label='Imagem' placeholder='URL da sua imagem'/>
     </div>
   );
 }
--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,10 +1,13 @@
 import './CampoTexto.css'
 
-const CampoTexto = () =>{
+const CampoTexto = (props) =>{
+    
+    const placeholderModificadado = `${props.placeholder}...`
+
     return (
         <div className="campo-texto">
-            <label>Nome</label>
-            <input/>
+            <label>{props.label}</label>
+            <input placeholder={placeholderModificadado}/>
         </div>
     )
 }
