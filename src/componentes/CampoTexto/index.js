dfd31d3d48a3ba1efc1edfdc9f0f17cedfb87b21
Renato Oliveira <renatog.oliveira17@gmail.com>
2023-06-20 11:53:51 -0300

    first componet created

---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,12 @@
+import './CampoTexto.css'
+
+const CampoTexto = () =>{
+    return (
+        <div className="campo-texto">
+            <label>Nome</label>
+            <input/>
+        </div>
+    )
+}
+
+export default CampoTexto
