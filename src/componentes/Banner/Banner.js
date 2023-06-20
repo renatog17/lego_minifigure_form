5facf1460f9447d53d615d6d7954e70829b1650f
Renato Oliveira <renatog.oliveira17@gmail.com>
2023-06-20 11:31:18 -0300

    banner created

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+function Banner() {
+    return(
+        <header className='banner'>
+            <img src="/imagens/banner_lego.jpg" alt="banner principal da página" ></img>
+        </header>
+    )
+}
+
+export default Banner
